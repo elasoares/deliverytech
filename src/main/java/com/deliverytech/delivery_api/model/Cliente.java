c33cd269b1d5e5755a87d1aeccbe21f6c8ffbe1f
elaine <elainehap@gmail.com>
2026-01-20 21:02:39 -0300

    Subindo para o remoto o controller cliente criado.

--- src/main/java/com/deliverytech/delivery_api/model/Cliente.java
+++ src/main/java/com/deliverytech/delivery_api/model/Cliente.java
@@ -6,6 +6,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -43,6 +45,7 @@ public class Cliente {
     private boolean ativo;  
 
     @OneToMany(mappedBy="clientes")
+    @JsonIgnore
     private List<Pedido> pedidos = new ArrayList<>();
 
     
